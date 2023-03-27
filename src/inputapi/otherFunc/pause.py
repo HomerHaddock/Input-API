bc1b680af7b903884331c1b4d76817b5480aa849
Homer Haddock <homerhaddock@proton.me>
2023-03-27 02:56:14 -0600

    Sorting the two unique functions into their own scripts

---
+++ src/inputapi/otherFunc/pause.py
@@ -0,0 +1,10 @@
+def standard():
+    input("Press ENTER to continue:")
+
+
+def showValue(*values):
+    print(*values)
+    input("Press ENTER to continue:")
+
+
+__all__ = ["standard", "showValue"]
