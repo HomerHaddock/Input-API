f6ca6531b1256990d229035292ddab1206000b83
Homer Haddock <homerhaddock@proton.me>
2023-06-02 23:10:56 -0600

    Added docstrings

--- src/inputapi/otherFunc/pause/_pause.py
+++ src/inputapi/otherFunc/pause/_pause.py
@@ -1,7 +1,17 @@
 from .. import clearScreen
 
 
-def standard(clearOnLoad: bool = False, clearWhenDone: bool = False):
+def standard(*, clearOnLoad: bool = False, clearWhenDone: bool = False):
+    """Pauses program until user presses ENTER.
+
+    This function will pause the program until the user presses ENTER.
+    This function will also clear the screen if specified.
+    Note: This function will not display values
+
+    Args:
+        clearOnLoad (bool, optional): Clears terminal before pausing. Defaults to False.
+        clearWhenDone (bool, optional): Clears terminal after user presses ENTER. Defaults to False.
+    """  # noqa: E501
     if clearOnLoad:
         clearScreen.auto()
     input("Press ENTER to continue:")
@@ -10,6 +20,15 @@ def standard(clearOnLoad: bool = False, clearWhenDone: bool = False):
 
 
 def showValue(*values, clearOnLoad: bool = False, clearWhenDone: bool = False):
+    """showValue pauses program after displaying values.
+
+    This function will pause the program after displaying values.
+    Values are only positional arguments.
+
+    Args:
+        clearOnLoad (bool, optional): Clears terminal before pausing. Defaults to False.
+        clearWhenDone (bool, optional): Clears terminal when finished. Defaults to False.
+    """  # noqa: E501
     if clearOnLoad:
         clearScreen.auto()
     print(*values)
