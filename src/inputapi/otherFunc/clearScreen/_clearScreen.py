4442b99eb2ef5fec8b3da500d670e218ea8de2fa
Homer Haddock <homerhaddock@proton.me>
2023-06-02 23:10:20 -0600

    Fixed docstrings

--- src/inputapi/otherFunc/clearScreen/_clearScreen.py
+++ src/inputapi/otherFunc/clearScreen/_clearScreen.py
@@ -2,7 +2,7 @@ import os as _os
 
 
 def auto():
-    """auto clears terminal without need for input
+    """Clears entire terminal automatically
 
     Based on the operating system this function will run the command used best for the terminal
     Windows will have "cls" used and all other operating systems will have "clear" run
@@ -10,11 +10,11 @@ def auto():
     (This only supports Windows, MacOS, & Linux)
 
     No input required
-    """
+    """  # noqa: E501
     _os.system("cls" if _os.name == "nt" else "clear")
 
 
-def manual(OS: str):
+def manual(*, OS: str):
     """manual clears terminal with the command based on the OS input
 
     Based on the operating system inputted it will run the terminal command for the specific
@@ -24,8 +24,12 @@ def manual(OS: str):
         OS (str): Operating system type for command (Not case-sensitive but relies on correct spelling)
 
     Raises:
-        OSError: This will only raise when the operating system specified is not supported
-    """
+        OSError: This will only raise when the operating system specified is not supported or null
+    """  # noqa: E501
+    
+    if not OS:
+        raise ValueError("OS cannot be empty")
+    
     OS = OS.lower()
     if OS == "windows":
         _os.system("cls")
