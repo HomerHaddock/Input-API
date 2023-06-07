916d9a4c23c12f373d941b27abe10f204fbae6e1
Homer Haddock <homerhaddock@proton.me>
2023-06-06 19:01:56 -0600

    Improved the maintanability of input validation Made output backwards compatible

--- src/inputapi/numerical/integer/_integer.py
+++ src/inputapi/numerical/integer/_integer.py
@@ -1,11 +1,24 @@
 import logging
 from ... import strings as _strings, otherFunc as _otherFunc
 
+def intHandle(input:str, allowNeg:bool, min:int|None, max:int|None) -> int|bool:
+    negCount:int = input.count('-')
+    input:str = input.replace('-', "")
+    input:str = '-' + input if negCount % 2 else input
 
-def intStat(number: str) -> list[int]:
-    neg = number.count("-")
-    return [neg]
-
+    inputInt:int = int(input)
+    
+    if min:
+        if inputInt < min:
+            logging.warning("Number [%s] below minimum [%s]" % (input, min))
+            return False
+    
+    if max:
+        if inputInt > max:
+            logging.warning("Number [%s] above maximum [%s]" % (input, max))
+            return False
+    
+    return inputInt
 
 def newLineInt(
     request: str = "Input an integer:",
@@ -31,35 +44,15 @@ def newLineInt(
     if max is not None:
         maxInput = len(str(max))
 
-    followsMin = False
-    followsMax = False
-    followsNeg = False
-    while not (followsMin and followsMax and followsNeg):
+    validInput = False
+    while validInput is not True:
         user = _strings.sameLineStr(
             ">", minLength=minInput, maxLength=maxInput, allowOnly=allow
         )
-        stat = intStat(user)
-        user.replace("-", "")
-        if stat[0] % 2 == 1:
-            user = "-" + user
-        user = int(user)
-        if min is not None:
-            if user < min:
-                logging.warning(f"Number [{user}] below minimum [{min}]")
-                continue
-        followsMin = True
-        if max is not None:
-            if user > max:
-                logging.warning(f"Number [{user}] above maximum [{max}]")
-                continue
-        followsMax = True
-        if stat[0] % 2 == 1:
-            if not allowNeg:
-                logging.warning(
-                    f"Number [{user}] is negative, negative numbers are disabled for this input"  # noqa: E501
-                )
-                continue
-        followsNeg = True
+        
+        if type(result:=intHandle(user, allowNeg, min, max)) is int:
+            user = result
+            validInput = True
 
     if clearWhenDone:
         _otherFunc.clearScreen.auto()
@@ -88,35 +81,15 @@ def sameLineInt(
     if max is not None:
         maxInput = len(str(max))
 
-    followsMin = False
-    followsMax = False
-    followsNeg = False
-    while not followsMin or not followsMax or not followsNeg:
+    validInput = False
+    while validInput is not True:
         user = _strings.sameLineStr(
             ">", minLength=minInput, maxLength=maxInput, allowOnly=allow
         )
-        stat = intStat(user)
-        user.replace("-", "")
-        if stat[0] % 2 == 1:
-            user = "-" + user
-        user = int(user)
-        if min is not None:
-            if user < min:
-                logging.warning(f"Number [{user}] below minimum [{min}]")
-                continue
-        followsMin = True
-        if max is not None:
-            if user > max:
-                logging.warning(f"Number [{user}] above maximum [{max}]")
-                continue
-        followsMax = True
-        if stat[0] % 2 == 1:
-            if not allowNeg:
-                logging.warning(
-                    f"Number [{user}] is negative, negative numbers are disabled for this input"  # noqa: E501
-                )
-                continue
-        followsNeg = True
+        
+        if type(result:=intHandle(user, allowNeg, min, max)) is int:
+            user = result
+            validInput = True
 
     if clearWhenDone:
         _otherFunc.clearScreen.auto()
@@ -124,4 +97,4 @@ def sameLineInt(
     return user  # type: ignore
 
 
-__add__ = ["newLineInt", "sameLineInt", "intStat"]
+__add__ = ["newLineInt", "sameLineInt"]
