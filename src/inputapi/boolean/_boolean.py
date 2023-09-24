b3ff632ae42726d3e149f983c8b08e2c67b215f4
Homer Haddock <homerhaddock@proton.me>
2023-09-24 17:58:32 -0600

    Added default function Fixed bug with displaying request

--- src/inputapi/boolean/_boolean.py
+++ src/inputapi/boolean/_boolean.py
@@ -6,6 +6,7 @@ def yesNo(
     request: str = "Yes or no?",
     *,
     allowNumeric: bool = True,
+    default: int | str | None = None,
     clearOnLoad: bool = False,
     clearWhenDone: bool = False,
 ) -> bool:
@@ -14,7 +15,8 @@ def yesNo(
     For Boolean input you ask a request and get a Boolean representing the users response while repeating if the user gives an invalid response
 
     Args:
-        request (str): The question to be written in the terminal for the user to answer. Defaults to empty string.
+        request (str, optional): The question to be written in the terminal for the user to answer. Defaults to empty string.
+        default (int | str | None, optional): What will be returned if the user just presses enter. 0 or "y" means Y is default, 1 or "N" means N is default. Not case sensitive, and None means no default.
         allowNumeric (bool, optional): Allows the user to input a number along with the Y/n (Great for programs with a lot of numeric inputs). Defaults to True.
         clearOnLoad (bool, optional): Clears the terminal when running the function. Defaults to False.
         clearWhenDone (bool, optional): When the user gives the input it will clear the terminal. Defaults to False.
@@ -26,23 +28,36 @@ def yesNo(
     if clearOnLoad:
         _clearScreen()
 
-    if str != "":
+    if request != "":
         print(request)
+
+    display = ["y", "n"]
+    if default != None:
+        default = "yn".index(default.lower()) if isinstance(default, str) else default
+        display[default] = display[default].capitalize()
+    display = tuple(display)
 
     allowedInput = ["Y", "N", "y", "n"]
     if allowNumeric:
-        print("Numeric response allowed (Y=1, n=2)")
+        print("Numeric response allowed (%s=1, %s=2)" % display)
         allowedInput.append("1")
         allowedInput.append("2")
 
-    query = _sameLineStr("[Y/n]:", minLength=1,
-                         maxLength=1, allowOnly=''.join(allowedInput))
+    query = _sameLineStr(
+        "[%s/%s]:" % display,
+        minLength=0 if default != None else 1,
+        maxLength=1,
+        allowOnly="".join(allowedInput),
+    )
 
+    user = query.lower() in {"y", "1"}
+    if len(query) == 0:
+        user = True if not default else False
 
     if clearWhenDone:
         _clearScreen()
 
-    return query.lower() in {"y", "1"}
+    return user
 
 
 __all__ = ["yesNo"]
